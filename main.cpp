8781d2248badb110d8ae32b7a2b1d2a03f7326e9
Rohan Saha <13093012@student.uts.edu.au>
2020-01-01 02:26:33 +0600

    Bug fix; v1.0

--- main.cpp
+++ main.cpp
@@ -241,7 +241,7 @@ MOVESTATUS RollDiceProc (uint8_t* roll, int8_t& rollNb, const player_t* player)
                      event.key.code == sf::Keyboard::Space)
                 getInput = false;
 
-            /*DEBUG */
+            /*DEBUG
             else if (event.type == sf::Event::KeyPressed)
             {
                 if (event.key.code >= sf::Keyboard::Num1 &&
@@ -250,10 +250,11 @@ MOVESTATUS RollDiceProc (uint8_t* roll, int8_t& rollNb, const player_t* player)
                     roll[rollNb] = event.key.code - sf::Keyboard::Num1 + 1;
                     getInput = false;
                 }
-            }
+            }*/
+
         }
 
-        //roll[rollNb] = Game_RollDice();
+        roll[rollNb] = Game_RollDice();
 
         printf("Dice Score = %d", roll[rollNb]);
 
@@ -384,7 +385,6 @@ int main()
             RndrWindow.display();
 
             isSelectionValid = false;
-            move == GAME_ENDED;
 
             while (RndrWindow.waitEvent(event) && !isSelectionValid)
             {
